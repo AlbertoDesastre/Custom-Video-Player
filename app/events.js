3676d5d011e794f763cd940d06128f8f09a59fcc
AlbertoDesastre <elaprendiz6@gmail.com>
2022-09-11 15:12:13 +0100

    Bug Fixed SCSS: Events are shown in a good spot

--- app/events.js
+++ app/events.js
@@ -1,5 +1,5 @@
 export const events = [
-  { from: 2, to: 4, text: "EEEEEEEEEEEEEEEEEEEEEEEE" },
+  { from: 0, to: 60, text: "EEEEEEEEEEEEEEEEEEEEEEEE" },
   { from: 6, to: 10, text: "AAAAAAAAAAAAAAA" },
   { from: 8, to: 13, text: "UUUUUUUUUUUUUUUU" },
 ];
