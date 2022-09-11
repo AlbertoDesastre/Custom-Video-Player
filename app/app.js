299d173b9f08e4eee839bd3e00bc9d59ab46444e
AlbertoDesastre <elaprendiz6@gmail.com>
2022-09-11 15:06:04 +0100

    Modularize Files: Separated controllers.js from app.js

--- app/app.js
+++ app/app.js
@@ -3,25 +3,16 @@ import {
   getEventsMatchingVideoCurrentSecond,
 } from "./helpers.js";
 
-const app = document.querySelector("main");
 const video = document.querySelector("video");
-const play = document.querySelector(".play");
-const pause = document.querySelector(".pause");
-const fullscreen = document.querySelector(".fullscreen");
 const progressBar = document.querySelector("progress");
 const current_time = document.querySelector(".current_time");
 const video_duration = document.querySelector(".video_duration");
-const everyControl = document.querySelector("#section_controllers");
 const eventSection = document.querySelector("#list_events");
 
-let isItReady = false;
-let isItPlaying = false;
-let hideAndUnhide;
 let currentEvents = [];
 
 const readyToPlay = () => {
   video_duration.textContent = durationToSeconds(video.duration);
-  isItReady = true;
 };
 
 if (video.readyState >= 2) {
@@ -30,48 +21,7 @@ if (video.readyState >= 2) {
   video.addEventListener("loadeddata", readyToPlay);
 }
 
-/* Fullscreen management */
-/* Pending to create a function by itself and then call it when clicked */
-fullscreen.addEventListener("click", () => {
-  if (!document.fullscreenElement) {
-    /* This is a promise. Pending to study promises. */
-    app.requestFullscreen().catch((error) => console.error(error));
-  } else {
-    document.exitFullscreen().catch((error) => console.error(error));
-  }
-});
-
-/* Play and pause management */
-play.addEventListener("click", () => {
-  isItPlaying = true;
-  video.play();
-  play.hidden = true;
-  pause.hidden = false;
-});
-
-/* Shows controller if the mouse it's moved */
-video.addEventListener("mousemove", () => {
-  if (isItReady && isItPlaying) {
-    everyControl.classList.remove("please_hide");
-    /* This clearTimeout must exists. If it wasn't here the hideAndUnhide
-  function would still be running every 2 seconds after the mouse is moved.
-  This would result in the controllers menu "clipping" */
-    clearTimeout(hideAndUnhide);
-    hideAndUnhide = setTimeout(() => {
-      everyControl.classList.add("please_hide");
-    }, 2000);
-  }
-});
-
-pause.addEventListener("click", () => {
-  isItPlaying = false;
-  video.pause();
-  play.hidden = false;
-  pause.hidden = true;
-});
-
 /* Video's current time management */
-
 video.addEventListener("timeupdate", () => {
   /*  console.log(durationToSeconds(video.currentTime)); */
   current_time.textContent = durationToSeconds(video.currentTime);
@@ -92,7 +42,6 @@ const setEvents = (events) => {
 
   /* The join down there basically separates every string of an array, that way the commas
 won't be shown. */
-
   eventSection.innerHTML = `<ul>
       ${currentEvents
         .map((evento) => {
@@ -101,15 +50,5 @@ won't be shown. */
         .join("")}
       </ul>`;
 };
-
-/* Video's progress status management */
-
-progressBar.addEventListener("click", (e) => {
-  const whereDidTheUserClickOnProgressBar = e.pageX - progressBar.offsetLeft;
-  const value =
-    (whereDidTheUserClickOnProgressBar * progressBar.max) /
-    progressBar.offsetWidth;
-  video.currentTime = value;
-});
 
 /* Showing events */
